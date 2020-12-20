b921026a0ecd10d164cac3798f72a1dbe1251df5
Mathias Le Faouder <mathias.lefaouder@outlook.fr>
2020-12-20 15:50:05 +0100

    board + main

--- board.py
+++ board.py
@@ -1,4 +1,5 @@
 from cell import Cell
+from peon import Chef, Assasin, Necromobile, Diplomate, Militant, Reporter
 
 
 class Board:
@@ -9,8 +10,81 @@ class Board:
         self._teams = []
         self._selected_cell = None
 
-    def move(self):
-        pass
+    def move(self, x: int, y: int):
+        if self._selected_cell is not None:
+            cell = self._cells[x][y]
+            cell.peons = self._selected_cell.peons
+            self._selected_cell.peons = None
 
-    def get_empty_cells(self):
-        pass
+    def select_peons(self, x: int, y: int):
+        cell = self._cells[x][y]
+        if self._selected_cell != cell:
+            if self._selected_cell is None:
+                self._selected_cell = None
+            else:
+                self.move(x, y)
+                self._selected_cell = None
+        else:
+            self._selected_cell = None
+
+    def init_board(self):
+        team1 = 'Yellow'
+        team2 = 'Green'
+        team3 = 'Blue'
+        team4 = 'Red'
+
+        """ team 1 """
+        self._cells[0][0].peons = Chef(team1)
+        self._cells[0][1].peons = Assasin(team1)
+        self._cells[1][0].peons = Reporter(team1)
+        self._cells[1][1].peons = Diplomate(team1)
+        self._cells[2][2].peons = Necromobile(team1)
+        self._cells[0][2].peons = Militant(team1)
+        self._cells[1][2].peons = Militant(team1)
+        self._cells[2][0].peons = Militant(team1)
+        self._cells[2][1].peons = Militant(team1)
+
+        """ team 2 """
+        self._cells[0][8].peons = Chef(team2)
+        self._cells[0][7].peons = Assasin(team2)
+        self._cells[1][8].peons = Reporter(team2)
+        self._cells[1][7].peons = Diplomate(team2)
+        self._cells[2][6].peons = Necromobile(team2)
+        self._cells[0][6].peons = Militant(team2)
+        self._cells[1][6].peons = Militant(team2)
+        self._cells[2][8].peons = Militant(team2)
+        self._cells[2][7].peons = Militant(team2)
+
+        """ team 3 """
+        self._cells[8][0].peons = Chef(team3)
+        self._cells[8][1].peons = Assasin(team3)
+        self._cells[7][0].peons = Reporter(team3)
+        self._cells[7][1].peons = Diplomate(team3)
+        self._cells[6][2].peons = Necromobile(team3)
+        self._cells[8][2].peons = Militant(team3)
+        self._cells[7][2].peons = Militant(team3)
+        self._cells[6][0].peons = Militant(team3)
+        self._cells[6][1].peons = Militant(team3)
+
+        """ team 4 """
+        self._cells[8][8].peons = Chef(team4)
+        self._cells[8][7].peons = Assasin(team4)
+        self._cells[7][8].peons = Reporter(team4)
+        self._cells[7][7].peons = Diplomate(team4)
+        self._cells[6][6].peons = Necromobile(team4)
+        self._cells[8][6].peons = Militant(team4)
+        self._cells[7][6].peons = Militant(team4)
+        self._cells[6][8].peons = Militant(team4)
+        self._cells[6][7].peons = Militant(team4)
+
+    def get_selected_cell(self):
+        return self._selected_cell
+
+    def set_selected_cell(self, selected_cell: Cell):
+        self._selected_cell = selected_cell
+
+    def get_cells(self):
+        return self._cells
+
+    def set_cells(self, cells: []):
+        self._cells = cells
--- main.py
+++ main.py
@@ -1,18 +1,46 @@
 import tkinter
 
+from board import Board
 from cell import Cell
+
+
+def click(x: int, y: int):
+    if board.get_selected_cell() is not None:
+        pos_x = board.get_selected_cell().get_pos_x()
+        pos_y = board.get_selected_cell().get_pos_y()
+    board.select_peons(x, y)
+
+    if 'pos_x' in locals():
+        display_cell(cells[pos_x][pos_y])
+    display_cell(cells[x][y])
+
+
+def display_cell(cell: Cell):
+    x = cell.get_pos_x()
+    y = cell.get_pos_y()
+
+    color = cell.peons.color if cell.peons is not None else 'white'
+
+    btn = tkinter.Button(fenetre, bg=color, borderwidth=5, command=lambda x=cell.get_pos_x(), y=cell.get_pos_y(): click(x, y))
+
+    btn.grid(row=x, column=y)
+
 
 cells = []
 for i in range(9):
     row = []
     for j in range(9):
-        position = [i, j]
-        row.append(Cell(peon=None, position=position))
+        row.append(Cell(peon=None, position=[i, j]))
     cells.append(row)
 
 if __name__ == '__main__':
     fenetre = tkinter.Tk()
     fenetre.title('Djambi')
-    # TODO: display cell in board
+    board = Board(cells)
+    board.init_board()
+
+    for rowCells in cells:
+        for row in rowCells:
+            display_cell(row)
 
     fenetre.mainloop()
